86ee4303c9eb5453d3c00b073c84391b0bbdc587
shahzaib2k02 <k213819@nu.edu.pk>
2024-12-18 20:39:15 +0500

    cursor

--- src/components/WorKCardData.js
+++ src/components/WorKCardData.js
@@ -1,5 +1,6 @@
 import pro1 from '../assets/pro1.jpeg';
 import pro2 from '../assets/pro2.png';
+import pro3 from '../assets/pro3.jpg';
 
 const ProjectCardData = [
     {
@@ -12,6 +13,12 @@ const ProjectCardData = [
         imgsrc: pro2,
         title: 'Meeting App',
         text: 'A Meeting app design to manage all your meetings schedules, provide you with notes, providing notification bell and updates on missed meetings.',
+        view: 'https://github.com/k213819/Mern-projects'
+    },
+    {
+        imgsrc: pro3,
+        title: 'Tour and travel app',
+        text: 'Travelling booking app where you view tours, search on preference, book your favourties one.',
         view: 'https://github.com/k213819/Mern-projects'
     }
 ]
