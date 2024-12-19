f13fbd545e36b7cf7c91885e1689abd0195e31e1
shahzaib2k02 <k213819@nu.edu.pk>
2024-12-19 19:37:52 +0500

    Fix deployment issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import './index.css';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Loading from './components/Loading';
 import ScrollToTop from './components/ScrollToTop';
 
@@ -12,17 +12,15 @@ const Project = lazy(() => import('./routes/Project'));
 
 function App() {
   return (
-    <Router basename="/Portfolio">
-      <Suspense fallback={<Loading />}>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/project" element={<Project />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <Suspense fallback={<Loading />}>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </Suspense>
   );
 }
 
